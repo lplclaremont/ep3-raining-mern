b70d208accc35dbe9d6df08e8d081f0ed6fb2439
csanann <csanann@gmail.com>
2023-07-05 16:00:47 +0100

    Issues resolved

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,10 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import LocationsForm from './components/locationsForm/LocationsForm';
 
 function App() {
-  const cityOptions = [
-    { label: 'Brighton, UK', value: 'brighton' },
-    { label: 'Lisbon, Portugal', value: 'lisbon' },
-    { label: 'Valencia, Spain', value: 'valencia' },
-    { label: 'Riga, Latvia', value: 'riga' },
-    { label: 'Santorini, Greece', value: 'santorini' }
-  ];
-
-  const [count, setCount] = useState(0)
-
-  const appTitle = "WeatherWhisper App";
 
   return (
     <>
@@ -27,20 +16,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>{appTitle}</h1>
+      <h1>WeatherWhisper App</h1>
       <LocationsForm />
-      <div className="card">
-        <LocationsForm cityOptions={cityOptions} />
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
     </>
   );
 }
--- frontend/src/components/locationsForm/LocationsForm.jsx
+++ frontend/src/components/locationsForm/LocationsForm.jsx
@@ -1,18 +1,26 @@
 // file: frontend/src/components/locationsForm/LocationsForm.jsx
 
-import React from 'react';
-
-function LocationsForm({ cityOptions, selectedCity, handleCityChange }) {
+function LocationsForm() {
   return (
     <div>
       <label htmlFor="city-select">Select a city:</label>
-      <select id="city-select" value={selectedCity} onChange={handleCityChange}>
+      <select id="city-select">
         <option value="">Select a city</option>
-        {cityOptions.map((city) => (
-          <option key={city.value} value={city.value}>
-            {city.label}
+        <option key='brighton' value='brighton'>
+          Brighton, UK
           </option>
-        ))}
+          <option key='lisbon' value='lisbon'>
+          Lisbon, Portugal
+          </option>
+          <option key='valencia' value='valencia'>
+          Valencia, Spain
+          </option>
+          <option key='riga' value='riga'>
+          Riga, Latvia
+          </option>
+          <option key='santorini' value='santorini'>
+          Santorini, Greece
+          </option>
       </select>
       </div>
   );
