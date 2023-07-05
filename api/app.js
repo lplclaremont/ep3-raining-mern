9ea53f18fb12763987477305c2dad92475949fc3
Caroline Evans <carolineevans261@gmail.com>
2023-07-05 14:27:44 +0100

    added route and controller folders

--- api/app.js
+++ api/app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req,res) => {
-    res.send('Hello world!');
-});
+// app.get('/', (req,res) => {
+//     res.send('Hello world!');
+// });
+
+const weatherRouter = require('./routes/weather')
+
+app.use('/weather', weatherRouter)
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
