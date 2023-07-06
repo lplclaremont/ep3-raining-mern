719521fa08a6e454803feb0bb2089c4bee31a347
hanguyen <lotusviet.uk@gmail.com>
2023-07-06 12:34:13 +0100

    updated utils/processData.js to get the API response as expected

--- api/utils/processData.js
+++ api/utils/processData.js
@@ -1,27 +1,27 @@
 const processData = (data) => {
   const processedData = {
-    'lat': data.lat,
-    'lon': data.lon,
-    'daily': [
-      {
-        'dt': data.daily[0].dt,
-        // and the rest...
-      }, 
-      {
-
-      }, 
-      {
-
-      }, 
-      {
-
-      }, 
-      {
-
-      }
-    ]
+    lat: data.lat,
+    lon: data.lon,
+    daily: [],
+  };
+  for (let i = 0; i < 3; i++) {
+    const dailyData = {
+      dt: data.daily[i].dt,
+      temp: { day: data.daily[i].temp.day },
+      weather: [
+        {
+          id: data.daily[i].weather[0].id,
+          main: data.daily[i].weather[0].main,
+          description: data.daily[i].weather[0].description,
+        },
+      ],
+      clouds: data.daily[i].clouds,
+      pop: data.daily[i].pop,
+    };
+    processedData.daily.push(dailyData);
   }
+
   return processedData;
-}
+};
 
 module.exports = processData;
