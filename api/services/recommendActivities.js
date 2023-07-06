a2ead110ae85ba5366a24024793d678732ca2ba8
Caroline Evans <carolineevans261@gmail.com>
2023-07-06 11:50:59 +0100

    refactored to leave processed data unchanged and ONLY add the activity (before it was filtering out some of the original data but that shouldnt be part of the scope of this function)

--- api/services/recommendActivities.js
+++ api/services/recommendActivities.js
@@ -1,20 +1,9 @@
 
 const recommendActivities = (processedWeatherData) => {
-    let dailyArray = [];
-    processedWeatherData.daily.forEach((day) => {
-        console.log(`weather id: ${day.weather[0].id}\nweather main: ${day.weather[0].main}\nweather description: ${day.weather[0].description}\nclouds: ${day.clouds}\npop: ${day.pop}`)
-        dayObject = {
-            "dt": day.dt,
-            "temp": day.temp,
-            "weather": day.weather,
-            'activity': null
-        }
-        dailyArray.push(dayObject);
-    });
+    let dailyArray = processedWeatherData.daily;
     
     const outdoorActivities = ["Beach", "Sightseeing", "Sports"];
     const indoorActivities = ["Museums", "Shopping", "Eating"];
-
 
     dailyArray.sort((a, b) => b.temp.day - a.temp.day);
     const rainyDays = dailyArray.filter((day) => day.weather[0].main === 'Rain');
@@ -27,9 +16,8 @@ const recommendActivities = (processedWeatherData) => {
         day.activity = indoorActivities[index];
       });
     
-    dailyArray = [...nonRainyDays, ...rainyDays];
+    dailyArray = nonRainyDays.concat(rainyDays);
     dailyArray.sort((a, b) => a.dt - b.dt);
-
 
     return {"daily": dailyArray};
 };
