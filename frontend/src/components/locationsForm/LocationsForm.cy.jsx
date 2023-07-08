ac84e399af0d8ff014b1375a87f1768dc1efe419
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-07-08 21:29:26 +0100

    adding a test for locations form sending the correct GET request

--- frontend/src/components/locationsForm/LocationsForm.cy.jsx
+++ frontend/src/components/locationsForm/LocationsForm.cy.jsx
@@ -6,4 +6,23 @@ describe("LocationsForm", () => {
     cy.get('[data-cy="city-dropdown"]').should('contain.text', "Brighton, UK")
     cy.get('[data-cy="city-dropdown"]').should('contain.text', "Valencia, Spain")
   })
+
+  it('calls the /weather endpoint with correct city', () => {
+    cy.intercept('GET', 'http://localhost:3000/weather/?city=valencia', (req) => {
+      req.reply({
+        statusCode: 200
+      });
+    }
+    ).as('getWeather');
+  
+    cy.mount(<LocationsForm />);
+  
+    cy.get('[data-cy=city-dropdown]').select('valencia');
+  
+    cy.contains('Generate').click();
+  
+    cy.wait('@getWeather')
+      .its('request.url')
+      .should('eq', 'http://localhost:3000/weather/?city=valencia');
+  });
 })
