2a0265324a58e8bd6b74da3155bf9a4d2aef22cb
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-07-10 16:44:28 +0100

    seems to be working

--- api/services/recommendActivities.js
+++ api/services/recommendActivities.js
@@ -5,20 +5,12 @@ const recommendActivities = (processedWeatherData, activities, userSelectedActiv
   userSelectedActivities.forEach((activity) => {
     activities[activity].chosen = true
   })
+  console.log(userSelectedActivities)
+  
+  console.log(dailyArray)
   // Sort the weather from best - worst days
-  sortBestWeather(dailyArray)
-  // Get the user's selected activities to front of the list
-  const prioritisedActivities = prioritiseUserSelection(activities)
-  // Assign the best activity to best day
-  assignActivities(dailyArray, prioritisedActivities, activities)
+  //sortBestWeather(dailyArray)
 
-  dailyArray.sort((a, b) => a.dt - b.dt); // Sort by date (ascending)
-  processedWeatherData.daily = dailyArray; // Update daily array with new activity data
-
-  return processedWeatherData;
-};
-
-const sortBestWeather = (dailyArray) => {
   dailyArray.sort((a, b) => {
     if (a.weather[0].id < b.weather[0].id) {
       return 1; // Move worse weather i.e. lower id to the end
@@ -28,7 +20,32 @@ const sortBestWeather = (dailyArray) => {
     }
     return b.temp.day - a.temp.day; // Sort by temperature (hot to cold)
   });
-}
+
+  // Get the user's selected activities to front of the list
+  console.log(dailyArray)
+  const prioritisedActivities = prioritiseUserSelection(activities)
+  // Assign the best activity to best day
+  console.log(prioritisedActivities)
+
+  assignActivities(dailyArray, prioritisedActivities, activities)
+
+  dailyArray.sort((a, b) => a.dt - b.dt); // Sort by date (ascending)
+  processedWeatherData.daily = dailyArray; // Update daily array with new activity data
+
+  return processedWeatherData;
+};
+
+// const sortBestWeather = (dailyArray) => {
+//   dailyArray.sort((a, b) => {
+//     if (a.weather[0].id < b.weather[0].id) {
+//       return 1; // Move worse weather i.e. lower id to the end
+//     }
+//     if (a.weather[0].id > b.weather[0].id ) {
+//       return -1; // Move better weather i.e.  to the beginning
+//     }
+//     return b.temp.day - a.temp.day; // Sort by temperature (hot to cold)
+//   });
+// }
 
 const prioritiseUserSelection = (activities) => {
   return Object.entries(activities)
