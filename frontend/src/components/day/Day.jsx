6c9704cd8f5945415be3bed89e3588727d0e4e8e
hanguyen <lotusviet.uk@gmail.com>
2023-07-10 16:43:36 +0100

    added icon weather for everyday

--- frontend/src/components/day/Day.jsx
+++ frontend/src/components/day/Day.jsx
@@ -9,10 +9,15 @@ function Day ({ day }) {
 
   console.log(day);
   
+  const iconUrl = `http://openweathermap.org/img/wn/${day.weather[0].icon}.png`;
+
   return (
     <article className="day" key={day.dt}>
       <div className="date">{formattedDate}</div>
       <div className="temperature">{roundedTemperature}°C</div>
+      <div className="weather">
+        <img src={iconUrl} alt={day.weather[0].description} />
+      </div>
       <div className="weather">{day.weather[0].description}</div>
       <div className="activity">{day.activity}</div>
     </article>
