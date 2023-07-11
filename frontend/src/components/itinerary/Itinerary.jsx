d2d517cccaafcc968c09a00a157b25b522e310c6
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-07-11 15:06:50 +0100

    refactoring the Itinerary component and App.js so that Itinerary only rendered when the responseData not == []

--- frontend/src/components/itinerary/Itinerary.jsx
+++ frontend/src/components/itinerary/Itinerary.jsx
@@ -1,16 +1,11 @@
 import Day from "../day/Day";
 
 function Itinerary({ responseData }) {
-
-  if (responseData != []) {
-    return(
-      <div>
-        {responseData.map((day) => <Day day={day} key={day.dt}/>)}
-      </div>
+  return(
+    <div>
+      {responseData.map((day) => <Day day={day} key={day.dt}/>)}
+    </div>
     )
-  } else {
-    return (<p></p>)
-  }
 }
 
 export default Itinerary;
