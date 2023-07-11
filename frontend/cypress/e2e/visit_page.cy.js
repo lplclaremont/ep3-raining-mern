34d210c51eb6b2c05a9438cdb659b86f08095acc
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-07-11 20:30:24 +0100

    attempting to mock API call in my e2e test however the resulting data seems to be depending on the city selected

--- frontend/cypress/e2e/visit_page.cy.js
+++ frontend/cypress/e2e/visit_page.cy.js
@@ -1,5 +1,16 @@
+import mockApiResponse from './mockApiResponse';
+
 describe('Just visit e2e test', () => {
   it('should visit', () => {
-      cy.visit('/')
-  })
-})
+    cy.intercept('GET', `https://api.openweathermap.org/data/3.0/onecall*`, {
+      statusCode: 200,
+      body: mockApiResponse,
+    });
+
+    cy.visit('http://localhost:5174');
+    cy.get('[data-cy="city-dropdown"]').select('valencia')
+    .then(() => {
+      cy.contains('Generate').click();
+    })
+  });
+});
