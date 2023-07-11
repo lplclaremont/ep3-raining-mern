a89f8f1fa2dc1f934405af17816ce968d20dde8b
wimdavies <william.johnston.davies@gmail.com>
2023-07-11 12:02:34 +0100

    Hardcode fromDay and toDay values as MVP settings, to avoid broken behaviour on branch

--- api/controllers/weatherController.js
+++ api/controllers/weatherController.js
@@ -5,13 +5,19 @@ const recommendActivities = require('../services/recommendActivities.js');
 const weatherController = {
   getWeather: (req, res) => {
     let city = req.query.city;
+
+    let fromDay = 0;
+    let toDay = 2;
+    // let fromDay = req.query.fromDay;
+    // let toDay = req.query.toDay;
+
     let lat = cityLookup[city].lat;
     let lon = cityLookup[city].lon;
     const apiKey = res.locals.apiKey;
 
     fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&units=metric&appid=${apiKey}`)
       .then(response => response.json())
-      .then(data => processData(data))
+      .then(data => processData(data, fromDay, toDay))
       .then(processedData => recommendActivities(processedData))
       .then(recommendationsData => res.status(200).json(recommendationsData))
   }
