9581d4ca94c3e649e3fb7330f332fbbb7c626aaa
Caroline Evans <carolineevans261@gmail.com>
2023-07-11 15:58:08 +0100

    added regex email checker function

--- frontend/src/components/email/Email.jsx
+++ frontend/src/components/email/Email.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
 import './Email.css'; 
 
+function validateEmail(email) {
+  const emailRegex = /^[^s@]+@[^s@]+.[^s@]+$/;
+  return emailRegex.test(email);
+}
+
 function Email({ responseData }) {
   const [emailAddress, setEmailAddress] = useState('');
   const [clicked, setClicked] = useState(false);
+  const [incorrectEmail, setIncorrectEmail] = useState(false);
 
   const handleEmailChange = (event) => {
     setEmailAddress(event.target.value);
@@ -14,18 +20,25 @@ function Email({ responseData }) {
 
     console.log(responseData)
 
-    fetch(url, {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ 
-        emailAddress: emailAddress,
-        itinerary: responseData
-      })
-    }).catch(error => console.error(error));
+    if (validateEmail(emailAddress)) {
+      fetch(url, {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ 
+          emailAddress: emailAddress,
+          itinerary: responseData
+        })
+      }).catch(error => console.error(error));
+  
+      setClicked(true)
+      setIncorrectEmail(false);
+    } else {
+      setIncorrectEmail(true);
+    }
 
-    setClicked(true)
+    
 
   }
 
@@ -35,8 +48,9 @@ function Email({ responseData }) {
       {responseData.length > 0 && (
         <>
           <p>Want a copy of your itinerary? Add your email address here:</p>
-          <input type='email' onChange={handleEmailChange}></input>
+          <input type='email' onChange={handleEmailChange} required></input>
           <button className={`button-email ${clicked ? 'clicked' : ''}`} onClick={handleGenerateClick}> {clicked ? 'Email Sent!':'Send Email'}</button>
+          <p>{incorrectEmail ? 'Please enter a valid email address' : ''}</p>
         </>
       )}
     </div>
