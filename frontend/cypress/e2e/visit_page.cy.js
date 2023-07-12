19a927238a2c5e3d8d8128c9ac968c488bedc50e
wimdavies <william.johnston.davies@gmail.com>
2023-07-12 14:13:30 +0100

    Add cypress e2e test for DateRange selection

--- frontend/cypress/e2e/visit_page.cy.js
+++ frontend/cypress/e2e/visit_page.cy.js
@@ -1,5 +1,4 @@
 describe('Just visit e2e test', () => {
-
   it('should render 3 day display when no activities or dates selected', () => {
     cy.visit('http://localhost:5173');
     cy.get('[data-cy="city-dropdown"]').select('brighton');
@@ -14,12 +13,24 @@ describe('Just visit e2e test', () => {
     cy.visit('http://localhost:5173');
     cy.get('[data-cy="city-dropdown"]').select('valencia');
   
-    cy.contains('Generate').click();
     cy.contains('Beach').click();
     cy.contains('Museums').click();
+    cy.contains('Generate').click();
 
     cy.get('[data-cy="itinerary"]')
     cy.get('[data-cy="day-display"]').should('have.length', 3)
   });
-  
+
+  it('should render 1 day display when date range today-today selected', () => {
+    cy.visit('http://localhost:5173');
+    cy.get('[data-cy="city-dropdown"]').select('valencia');
+
+    cy.get('.react-datepicker-wrapper').click();
+    cy.get('[class*="today"]').click();
+    cy.get('[class*="today"]').click();
+
+    cy.contains('Generate').click();
+
+    cy.get('[data-cy="day-display"]').should('have.length', 1)
+  })
 });
