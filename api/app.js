8b3cda1a7cb69b0276956c9d941251c2ebf4d50c
Caroline Evans <carolineevans261@gmail.com>
2023-07-12 12:16:33 +0100

    added folder structure for controller testing but couldnt complete first emailController test with current app testing setup

--- api/app.js
+++ api/app.js
@@ -28,4 +28,4 @@ app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
 
-// module.exports = app;
+module.exports = app;
--- api/controllers/emailController.js
+++ api/controllers/emailController.js
@@ -1,28 +1,26 @@
 const formatDate = require('../utils/formatDate')
 const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: 'fromwhatevertheweather@gmail.com',
-    pass: 'shqbbwkeyvfagvjd'
-  }
-});
-
-
 const emailController = {
   sendEmail: (req, res) => {
     let emailAddress = req.body.emailAddress;
     let itinerary = req.body.itinerary;
 
-    console.log(emailAddress)
-    console.log(itinerary)
-
     let emailBody = `Your personal itinerary from Whatever The Weather!\n\n`
     itinerary.forEach((day) => {
       emailBody += `Date: ${formatDate(day.dt)}, Temp: ${day.temp.day.toFixed()}°C, Weather: ${day.weather[0].description}, Recommended activity: ${day.activity}\n\n `
     })
+
+    console.log(emailAddress)
     console.log(emailBody)
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'fromwhatevertheweather@gmail.com',
+        pass: 'shqbbwkeyvfagvjd'
+      }
+    });
 
     const mailOptions = {
       from: 'fromwhatevertheweather@gmail.com',
@@ -36,11 +34,10 @@ const emailController = {
      console.log(error);
       } else {
         console.log('Email sent: ' + info.response);
-        // do something useful
+        res.status(201).json({message: 'Email sent'})
       }
     });
 
-    
   }
 };
 
