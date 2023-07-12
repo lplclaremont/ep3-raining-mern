c974a4217c8cb49f88fb189a155081f91d369185
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-07-12 11:01:25 +0100

    changing cypress interception to mock the whole backend call to make sure it is working correctly

--- frontend/cypress/e2e/visit_page.cy.js
+++ frontend/cypress/e2e/visit_page.cy.js
@@ -1,14 +1,59 @@
 import mockApiResponse from './mockApiResponse';
 
 describe('Just visit e2e test', () => {
+  const mockResponseData = {
+    "daily": [
+    {
+      "dt": 1688990400,
+      "temp": {
+          "day": 29.77
+      },
+      "weather": [
+          {
+              "description": "clear sky"
+          }
+      ],
+      "activity": "Beach"
+    },
+    {
+      "dt": 1688990400,
+      "temp": {
+          "day": 18.57
+      },
+      "weather": [
+          {
+              "description": "cloudy"
+          }
+      ],
+      "activity": "Museums"
+    },
+    {
+      "dt": 1688990400,
+      "temp": {
+          "day": 22.2
+      },
+      "weather": [
+          {
+              "description": "clear sky"
+          }
+      ],
+      "activity": "Sightseeing"
+  }]
+  }
+
   it('should visit', () => {
-    cy.intercept('GET', `https://api.openweathermap.org/data/3.0/onecall*`, {
+    // cy.intercept('GET', "https://api.openweathermap.org/data/3.0/onecall*", {
+    //   statusCode: 200,
+    //   body: mockApiResponse,
+    // });
+
+    cy.intercept('GET', "http://localhost:3000/weather/?*", {
       statusCode: 200,
-      body: mockApiResponse,
+      body: mockResponseData,
     });
 
     cy.visit('http://localhost:5173');
-    cy.get('[data-cy="city-dropdown"]').select('phuket');
+    cy.get('[data-cy="city-dropdown"]').select('bournemouth');
   
     cy.contains('Generate').click();
   });
