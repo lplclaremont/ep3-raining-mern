8345b5e144b5ccf67952fdb8695133d8df450d4e
Will Davies <112558900+wimdavies@users.noreply.github.com>
2023-07-12 17:04:40 +0100

    Revert "messages acknowledge if the algorithm hasn't recommended an activity they requested"

--- frontend/src/components/itinerary/Itinerary.jsx
+++ frontend/src/components/itinerary/Itinerary.jsx
@@ -1,37 +1,15 @@
-import Day from '../day/Day';
-import { useState, useEffect } from 'react';
+import Day from "../day/Day";
 
-function Itinerary({ responseData, preferredActivities }) {
-  const [errorMessage, setErrorMessage] = useState("");
+function Itinerary({ responseData }) {
 
-  useEffect(() => {
-    const responseActivities = responseData.map((day) => day.activity);
-
-    const areAllRecommendationsFulfilled = preferredActivities.every((activity) => {
-      responseActivities.includes(activity)
-    })
-
-    if (!areAllRecommendationsFulfilled) {
-      setErrorMessage(
-        'Sorry, we could not accommodate all of the activities you selected. ' +
-          'We hope you still like our recommendations for your trip!'
-      );
-    } else {
-      setErrorMessage('');
-    }
-  }, [responseData, preferredActivities]);
-
-  if (responseData.length !== 0) {
-    return (
-      <div className='itinerary'>
-        {responseData.map((day) => (<Day day={day} key={day.dt} />))}
-        {errorMessage && (
-          <div className='error-message'>{errorMessage}</div>
-        )}
+  if (responseData != []) {
+    return(
+      <div>
+        {responseData.map((day) => <Day day={day} key={day.dt}/>)}
       </div>
-    );
+    )
   } else {
-    return <p></p>;
+    return (<p></p>)
   }
 }
 
