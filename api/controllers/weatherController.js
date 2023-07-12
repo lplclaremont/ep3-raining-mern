cc34cc9b8a15432d07d43c4c218f79af77f293b7
csanann <csanann@gmail.com>
2023-07-12 13:32:11 +0100

    Save local changes in weatherController.js before merge to main

--- api/controllers/weatherController.js
+++ api/controllers/weatherController.js
@@ -1,20 +1,35 @@
-const processData = require('../utils/processData.js');
-const cityLookup = require('../utils/cityLookup.js');
+//file: api/controllers/weatherController.js
+
+const fetch = require('node-fetch');
 const recommendActivities = require('../services/recommendActivities.js');
+const cityLookup = require('../data/cityLookup.js');
 
 const weatherController = {
-  getWeather: (req, res) => {
-    let city = req.query.city;
-    let lat = cityLookup[city].lat;
-    let lon = cityLookup[city].lon;
-    const apiKey = res.locals.apiKey;
+  getWeather: async (req, res) => {
+    try {
+      const { city, fromDay = 0, toDay = 2, activities } = req.query;
+      const { lat, lon } = cityLookup[city];
+      const apiKey = res.locals.apiKey;
 
-    fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&units=metric&appid=${apiKey}`)
-      .then(response => response.json())
-      .then(data => processData(data))
-      .then(processedData => recommendActivities(processedData))
-      .then(recommendationsData => res.status(200).json(recommendationsData))
+      const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&units=metric&appid=${apiKey}`);
+      const weatherData = await weatherResponse.json();
+
+      const processedData = processData(weatherData, fromDay, toDay);
+      const recommendationsData = recommendActivities(processedData, activities || [], userSelected);
+
+      res.status(200).json(recommendationsData);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'An error occurred while fetching weather data.' });
+    }
   }
 };
+
+function processData(data, fromDay, toDay) {
+  // Your data processing logic here
+  // ...
+
+  return processedData;
+}
 
 module.exports = weatherController;
